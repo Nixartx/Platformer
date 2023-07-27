8bc4e34b4f0949d609f8dd4e1d0fc358f3e4ec5f
Artyom Litvinenko <nixart986@gmail.com>
2023-07-27 15:20:00 +0300

    Added corpse destroy

---
+++ Assets/Scripts/DeadPrefab.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadPrefab : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    void Start()
+    {
+        StartCoroutine(StartCorpseDestroy());
+    }
+
+    IEnumerator StartCorpseDestroy()
+    {
+        yield return new WaitForSeconds(2);
+        StartCoroutine(CorpseDestroy());
+    }
+
+    IEnumerator CorpseDestroy()
+    {
+        Color color = _spriteRenderer.color;
+        
+        for (int i = 0; i < 6; i++)
+        {
+            yield return new WaitForSeconds(.5f);
+            color.a = color.a == 0 ? 1 : 0;
+            _spriteRenderer.color = color;
+        }
+        Destroy(gameObject);
+    }
+
+
+}
