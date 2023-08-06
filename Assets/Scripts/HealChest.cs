810f29dc7996c5376fe79c58d7ace91677f54be5
Artyom Litvinenko <nixart986@gmail.com>
2023-08-06 13:44:04 +0300

    Added animation to Heal Chest

--- Assets/Scripts/HealChest.cs
+++ Assets/Scripts/HealChest.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 
 public class HealChest : MonoBehaviour
 {
-    public int bonusHeal;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private GameObject _parentContainer;
+    [SerializeField] private int bonusHeal;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Health health = other.gameObject.GetComponent<Health>();
         if (other.gameObject.CompareTag("Player"))
         {
+            Health health = other.gameObject.GetComponent<Health>();
             health.SetHealth(bonusHeal);
-            Destroy(gameObject);
+            StartDestroy();
         }
     }
 
     private void Start()
     {
         GameManager.Instance.chestHealContainer.Add(gameObject,this);
+    }
+    
+    public void StartDestroy()
+    {
+        GameManager.Instance.chestHealContainer.Remove(gameObject);
+        _animator.SetTrigger("HealChestDestroy");
+    }
+
+    public void Destroy()
+    {
+        Destroy(_parentContainer);
     }
 }
